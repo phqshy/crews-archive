b18f6521c7d5e78fcdbd3d9548acc1f95cc00442
Fishy <87027056+phqshy@users.noreply.github.com>
2025-07-09 10:55:53 -0400

    Update database configs

--- database.py
+++ database.py
@@ -5,11 +5,12 @@ import psycopg2
 postgres_url = os.environ['POSTGRES_URL']
 postgres_username = os.environ['POSTGRES_USERNAME']
 postgres_password = os.environ['POSTGRES_PASSWORD']
-postgres_database = os.environ['POSTGRES_DATABASE']
+rmb_database_name = os.environ['RMB_DATABASE']
+discord_database_name = os.environ['DISCORD_DATABASE']
 
 
 rmb_database = psycopg2.connect(
-    f"host={postgres_url} dbname=archives user={postgres_username} password={postgres_password}")
+    f"host={postgres_url} dbname={rmb_database_name} user={postgres_username} password={postgres_password}")
 
 discord_database = psycopg2.connect(
-    f"host={postgres_url} dbname=discord user={postgres_username} password={postgres_password}")
+    f"host={postgres_url} dbname={discord_database_name} user={postgres_username} password={postgres_password}")
