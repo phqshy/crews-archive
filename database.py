062d9120f15ffcbb6bd4e0845456658937d9c74a
Fishy <87027056+phqshy@users.noreply.github.com>
2025-07-09 11:48:50 -0400

    Add secret file reading again

--- database.py
+++ database.py
@@ -4,7 +4,9 @@ import psycopg2
 
 postgres_url = os.environ['POSTGRES_URL']
 postgres_username = os.environ['POSTGRES_USERNAME']
-postgres_password = os.environ['POSTGRES_PASSWORD']
+postgres_password_file = os.environ['POSTGRES_PASSWORD']
+with open(postgres_password_file) as f:
+    postgres_password = f.readline()
 rmb_database_name = os.environ['RMB_DATABASE']
 discord_database_name = os.environ['DISCORD_DATABASE']
 
